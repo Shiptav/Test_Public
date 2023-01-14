2396ddd2fc5e7ea040ea8dd6e4c81624edc76e30
unknown <shiptav@mail.ru>
2023-01-14 22:04:07 +0300

    Home task 17

--- HT_17_7_3.py
+++ HT_17_7_3.py
@@ -1,22 +1,21 @@
-
-#money = input("Введите сумму депозита ")
-money = 100000
+money = input("Введите сумму депозита ")
+#money = 100000
 
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-deposit=[] #объявляем пустой список
+deposit=[ int(value*int(money)/100) for value in per_cent.values()]
 
-for value in per_cent.keys():  #пробегаем наш словарь
-    a=per_cent.get(value) # присваиваем переменной а, значение из ключа
-    b= (a*int(money))/100 # рассчитываем колличество денег за год
-    
-    deposit.append(int(b)) # добавляем рассчитанное выше значение в список
-print("deposit=",deposit) # выводим заполненный список напечать
+# for value in per_cent.values():  #пробегаем наш словарь извлекаем значения
+#     b= value*int(money)/100 # рассчитываем колличество денег за год
+#     deposit.append(int(b)) # добавляем рассчитанное выше значение в список deposit
+# print("deposit=",deposit) # выводим заполненный список 
 
-#max=0
-#for i in deposit:
-    #if i>max:
-        #max=i
-#print("Максимальная сумма, которую вы можете заработать", max)
+print("deposit=",deposit) # выводим заполненный список 
+
+# max=deposit[0]
+# for i in deposit:
+#     if i>max:
+#         max=i
+# print("Максимальная сумма, которую вы можете заработать", max)
     
 deposit_max = max(deposit) # ищем максимальное значение в списке
-print("Максимальная сумма, которую вы можете заработать", deposit_max)# выводим максимальное значение на печать
+print("Максимальная сумма, которую вы можете заработать", deposit_max)# выводим максимальное значение 
